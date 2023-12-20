f16a8bb9237b8ea9383eaebabfa29ac233dcffb0
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2023-12-20 00:31:54 -0300

    finalizado backend

--- backend/prisma/migrations/20231220030227_create_model_pizzaria/migration.sql
+++ backend/prisma/migrations/20231220030227_create_model_pizzaria/migration.sql
@@ -1,9 +1,21 @@
 -- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "categories" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "created_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updated_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
 );
@@ -15,8 +27,8 @@ CREATE TABLE "products" (
     "price" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "banner" TEXT NOT NULL,
-    "created_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updated_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "category_id" TEXT NOT NULL,
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
@@ -29,8 +41,8 @@ CREATE TABLE "orders" (
     "status" BOOLEAN NOT NULL DEFAULT false,
     "draft" BOOLEAN NOT NULL DEFAULT true,
     "name" TEXT,
-    "created_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updated_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
 );
@@ -39,8 +51,8 @@ CREATE TABLE "orders" (
 CREATE TABLE "items" (
     "id" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
-    "created_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updated_ad" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "order_id" TEXT NOT NULL,
     "product_id" TEXT NOT NULL,
 
