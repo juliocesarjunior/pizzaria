62dd9329edfad058941acc1a640d98c4bf0e7863
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2023-12-23 21:18:55 -0300

    area login

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native';
+
+export default function SignIn() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  function handleLogin(){
+    if(email === '' || password === ''){
+      return;
+    }
+    console.log(email)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logo.png')}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput 
+        placeholder='Email' 
+        style={styles.input} 
+        placeholderTextColor="#f0f0f0"
+        value={email}
+        onChangeText={setEmail}
+        >
+        </TextInput>
+        <TextInput 
+        placeholder='Senha' 
+        style={styles.input} 
+        placeholderTextColor="#f0f0f0" 
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+        >
+        </TextInput>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Acessar</Text>
+      </TouchableOpacity>
+      </View>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1d1d2e'
+  },
+  logo: {
+    marginBottom: 18,
+  },
+  inputContainer: {
+    width: '95%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 34,
+  },
+  input: {
+    width: '95%',
+    height: 40,
+    backgroundColor: '#101026',
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: '#fff',
+
+  },
+  button: {
+    width: '95%',
+    height: 40,
+    backgroundColor: '#3fffa3',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#101026'
+  }
+})
