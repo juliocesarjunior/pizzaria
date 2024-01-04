2c6d07eabaac40f99ee7a81eabab89e3310f9864
Julio Cesar Junior <julio.cesar3151@hotmail.com.br>
2024-01-04 17:30:32 -0300

    erro list item e remove item

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -137,6 +137,20 @@ export default function Order(){
 
   }
 
+  async function handleDeleteItem(item_id:string) {
+    const response = await api.delete('/order/remove', {
+     params:{
+       item_id: item_id
+      }
+    })
+    let removeItem = items.filter(item => {
+      return(item.id !== item_id)
+    })
+    
+    setItems(removeItem)
+  }
+
+
   return(
     <View style={styles.container}>
       
@@ -195,7 +209,7 @@ export default function Order(){
         style={{ flex: 1, marginTop: 24 }}
         data={items}
         keyExtractor={(item) => item.id }
-        renderItem={ ({ item }) =>  <ListItem data={item} /> }
+        renderItem={ ({ item }) =>  <ListItem data={item} deleteItem={handleDeleteItem} /> }
       />
 
 
